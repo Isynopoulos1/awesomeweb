ddcd1dce9989838ade05083030455e228c487300
Isela Valdez alarcón <72225396+Isynopoulos1@users.noreply.github.com>
2022-02-17 20:35:32 +0100

    hello3

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello from jS");
+ScrollReveal().reveal(
+  ".intro",
+
+  { delay: 1000, distance: "50px", duration: 1000 }
+);
+ScrollReveal().reveal(
+  ".work-1",
+
+  { delay: 600, distance: "50px", duration: 1000 }
+);
+ScrollReveal().reveal(
+  ".work-2",
+
+  { delay: 500, distance: "50px", duration: 1000 }
+);
+ScrollReveal().reveal(
+  ".work-3",
+
+  { delay: 400, distance: "50px", duration: 1000 }
+);
